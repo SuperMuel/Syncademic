075e6295ccb360c6004c0e16c68d25c9dfa1efd8
Samuel M. <supermuel@supermuel.fr>
2024-03-06 15:39:13 +0100

    Refactor GoogleTargetCalendarRepository to handle Google calendars and rename classes

--- syncademic_app/lib/repositories/google_target_calendar_repository.dart
+++ syncademic_app/lib/repositories/google_target_calendar_repository.dart
@@ -4,6 +4,12 @@ import '../models/target_calendar.dart';
 import 'target_calendar_repository.dart';
 import 'package:http/http.dart' as http;
 
+// TODO : All the google calendars are not yet target calendars. Those are just
+// Google calendars ! We need to rename this class to GoogleCalendarRepository
+// and TargetCalendarRepository to GoogleTargetCalendarRepository
+// and make them return Calendar instead of TargetCalendar
+// and handle access token elsewhere
+
 class GoogleTargetCalendarRepository implements TargetCalendarRepository {
   final CalendarApi _client;
   final String? accessToken;

1aca7f8712f7d0cdb4199251faef2e0b10289fdd
Samuel M. <supermuel@supermuel.fr>
2024-03-15 11:30:39 +0100

    Test target_calendar

--- syncademic_app/lib/models/target_calendar.dart
+++ syncademic_app/lib/models/target_calendar.dart
@@ -5,15 +5,11 @@ class TargetCalendar extends Equatable {
   final ID id;
   final String title;
   final String? accessToken;
-  //TODO(SuperMuel) also get refresh token
-
-  final bool? backendHasAccess;
 
   const TargetCalendar({
     required this.id,
     required this.title,
     this.accessToken,
-    this.backendHasAccess,
   });
 
   @override

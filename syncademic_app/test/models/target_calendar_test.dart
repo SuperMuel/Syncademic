c85c9517a1df131f878643134a8f6a2a373857d1
SuperMuel <69467005+SuperMuel@users.noreply.github.com>
2024-04-17 23:35:59 +0200

    Refine tests

--- syncademic_app/test/models/target_calendar_test.dart
+++ syncademic_app/test/models/target_calendar_test.dart
@@ -5,68 +5,71 @@ import 'package:syncademic_app/models/target_calendar.dart';
 void main() {
   const id = ID.fromString('test_id');
   const title = 'Test Title';
+  const accountOwnerUserId = 'accountOwnerUserId';
+
+  TargetCalendar createTargetCalendar({
+    ID id = id,
+    String title = title,
+    String accountOwnerUserId = accountOwnerUserId,
+  }) =>
+      TargetCalendar(
+        id: id,
+        title: title,
+        accountOwnerUserId: accountOwnerUserId,
+      );
 
   test('should create a TargetCalendar instance', () {
     const targetCalendar = TargetCalendar(
       id: id,
       title: title,
+      accountOwnerUserId: accountOwnerUserId,
     );
 
     expect(targetCalendar.id, id);
     expect(targetCalendar.title, title);
+    expect(targetCalendar.accountOwnerUserId, accountOwnerUserId);
   });
 
   group('equals', () {
     test('should be equal to another TargetCalendar with the values', () {
-      const targetCalendar1 = TargetCalendar(
-        id: id,
-        title: title,
-      );
-      const targetCalendar2 = TargetCalendar(
-        id: id,
-        title: title,
-      );
+      final targetCalendar1 = createTargetCalendar();
+      final targetCalendar2 = createTargetCalendar();
       expect(targetCalendar1, equals(targetCalendar2));
     });
 
     test('should not be equal to another TargetCalendar with a different id',
         () {
-      const id1 = ID.fromString('test_id1');
-      const targetCalendar1 = TargetCalendar(
-        id: id1,
-        title: title,
-      );
-      const targetCalendar2 = TargetCalendar(
-        id: id,
-        title: title,
-      );
+      final targetCalendar1 =
+          createTargetCalendar(id: const ID.fromString("A"));
+      final targetCalendar2 =
+          createTargetCalendar(id: const ID.fromString("B"));
+
       expect(targetCalendar1, isNot(equals(targetCalendar2)));
     });
 
     test('should not be equal to another TargetCalendar with a different title',
         () {
-      const title1 = 'Test Title 1';
-      const targetCalendar1 = TargetCalendar(
-        id: id,
-        title: title1,
-      );
-      const targetCalendar2 = TargetCalendar(
-        id: id,
-        title: title,
-      );
+      final targetCalendar1 = createTargetCalendar(title: 'A');
+      final targetCalendar2 = createTargetCalendar(title: 'B');
+
       expect(targetCalendar1, isNot(equals(targetCalendar2)));
     });
 
-    test('should have the same hash code for TargetCalendars with the values',
+    test(
+        'should not be equal to another TargetCalendar with a different accountOwnerUserId',
         () {
-      const targetCalendar1 = TargetCalendar(
-        id: id,
-        title: title,
-      );
-      const targetCalendar2 = TargetCalendar(
-        id: id,
-        title: title,
-      );
+      final targetCalendar1 = createTargetCalendar(accountOwnerUserId: 'A');
+      final targetCalendar2 = createTargetCalendar(accountOwnerUserId: 'B');
+
+      expect(targetCalendar1, isNot(equals(targetCalendar2)));
+    });
+
+    test(
+        'should have the same hash code for TargetCalendars with the same values',
+        () {
+      final targetCalendar1 = createTargetCalendar();
+      final targetCalendar2 = createTargetCalendar();
+
       expect(targetCalendar1.hashCode, equals(targetCalendar2.hashCode));
     });
   });

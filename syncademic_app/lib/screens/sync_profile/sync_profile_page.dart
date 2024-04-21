180e95c9fb6d1a4be33e4f3d53d4038d27cacb1b
SuperMuel <69467005+SuperMuel@users.noreply.github.com>
2024-04-22 01:19:11 +0200

    Fix imports

--- syncademic_app/lib/screens/sync_profile/sync_profile_page.dart
+++ syncademic_app/lib/screens/sync_profile/sync_profile_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:syncademic_app/widgets/target_calendar_card.dart';
+import '../../widgets/target_calendar_card.dart';
 
 import '../../models/sync_profile.dart';
 import '../../models/sync_profile_status.dart';

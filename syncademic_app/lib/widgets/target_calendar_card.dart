d002528f5cc4d62e539a64b174cd505e1da7c95b
SuperMuel <69467005+SuperMuel@users.noreply.github.com>
2024-04-21 17:32:13 +0200

    Fix edit icon position

--- syncademic_app/lib/widgets/target_calendar_card.dart
+++ syncademic_app/lib/widgets/target_calendar_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import '../models/target_calendar.dart';
 
@@ -17,55 +16,61 @@ class TargetCalendarCard extends StatelessWidget {
     this.maxWidth = 500,
   });
 
-  //TODO : stack this
-  // if (showEditIcon)
-  //   IconButton(
-  //     icon: const Icon(Icons.edit),
-  //     onPressed: onPressed,
-  //   ),
-
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
       constraints: BoxConstraints(maxWidth: maxWidth),
-      child: Card(
-        elevation: 2,
-        child: InkWell(
-          onTap: onPressed,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                //image icon
-                Image.asset(
-                  "assets/icons/google_calendar_icon_1024x1024.png",
-                  width: 48,
-                  height: 48,
-                ),
-                const SizedBox(width: 16),
-                Flexible(
-                  fit: FlexFit.loose,
-                  child: Column(
-                    children: [
-                      Text(
-                        targetCalendar.title,
-                        style: Theme.of(context).textTheme.titleLarge,
+      child: Stack(
+        children: [
+          Card(
+            elevation: 2,
+            child: InkWell(
+              onTap: onPressed,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    //image icon
+                    Image.asset(
+                      "assets/icons/google_calendar_icon_1024x1024.png",
+                      width: 48,
+                      height: 48,
+                    ),
+                    const SizedBox(width: 16),
+                    Flexible(
+                      fit: FlexFit.loose,
+                      child: Column(
+                        children: [
+                          Text(
+                            targetCalendar.title,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          if (targetCalendar.description != null) ...[
+                            Text(
+                              targetCalendar.description!,
+                              style: Theme.of(context).textTheme.bodySmall,
+                            )
+                          ]
+                        ],
                       ),
-                      if (targetCalendar.description != null) ...[
-                        Text(
-                          targetCalendar.description!,
-                          style: Theme.of(context).textTheme.bodySmall,
-                        )
-                      ]
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+          if (showEditIcon)
+            Positioned(
+              top: 0,
+              right: 0,
+              child: IconButton(
+                icon: const Icon(Icons.edit),
+                onPressed: onPressed,
+              ),
+            ),
+        ],
       ),
     );
   }

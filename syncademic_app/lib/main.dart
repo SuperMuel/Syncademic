03be2bc811fe31a23d8a45db21dc8f79c6175fb9
Samuel Mallet <69467005+SuperMuel@users.noreply.github.com>
2024-09-24 20:36:19 +0200

    Configure Firebase AppCheck

--- syncademic_app/lib/main.dart
+++ syncademic_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -43,6 +45,28 @@ void main() async {
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  if (dotenv.env['RECAPTCHA_V3_SITE_KEY'] == null) {
+    throw Exception('RECAPTCHA_V3_SITE_KEY environment variable is not set.');
+  }
+
+  await FirebaseAppCheck.instance.activate(
+    webProvider: ReCaptchaV3Provider(dotenv.env['RECAPTCHA_V3_SITE_KEY']!),
+
+    // Default provider for Android is the Play Integrity provider. You can use the "AndroidProvider" enum to choose
+    // your preferred provider. Choose from:
+    // 1. Debug provider
+    // 2. Safety Net provider
+    // 3. Play Integrity provider
+    androidProvider: AndroidProvider.debug,
+    // Default provider for iOS/macOS is the Device Check provider. You can use the "AppleProvider" enum to choose
+    // your preferred provider. Choose from:
+    // 1. Debug provider
+    // 2. Device Check provider
+    // 3. App Attest provider
+    // 4. App Attest provider with fallback to Device Check provider (App Attest provider is only available on iOS 14.0+, macOS 14.0+)
+    appleProvider: AppleProvider.debug,
   );
 
   final googleSignIn = GoogleSignIn(

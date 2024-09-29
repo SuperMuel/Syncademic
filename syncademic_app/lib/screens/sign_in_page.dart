b3f717d9b73b453d57447dfd99a9cc4f46c3bbd8
Samuel Mallet <69467005+SuperMuel@users.noreply.github.com>
2024-09-29 20:06:07 +0200

    simple login screen

--- syncademic_app/lib/screens/sign_in_page.dart
+++ syncademic_app/lib/screens/sign_in_page.dart
@@ -31,14 +31,56 @@ class LogoAndSyncademic extends StatelessWidget {
           ),
           const SizedBox(width: 16),
           Text(
-            'Syncademic',
-            style: GoogleFonts.inter(
+            'Login',
+            style: GoogleFonts.roboto(
               fontSize: 32,
               fontWeight: FontWeight.w600,
               color: const Color(0xff16314d),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class GoogleSignInButton extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const GoogleSignInButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double
+            .infinity, // This line ensures the button takes all available width
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(32),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment
+              .center, // This line centers the content horizontally
+          children: [
+            Image.asset(
+              'assets/icons/google_icon_128.png', // Your Google logo asset path
+              height: 20, // Adjust height as needed
+              width: 20, // Adjust width as needed
+            ),
+            const SizedBox(width: 10),
+            Text(
+              'Sign in with Google',
+              style: GoogleFonts.roboto(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Colors.black54,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -63,21 +105,44 @@ class SignInPage extends StatelessWidget {
         builder: (context, state) {
           return Scaffold(
             body: SafeArea(
+                child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(32.0),
                 child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: Image.asset('assets/illustrations/little_boy.jpeg'),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child:
+                          Image.asset('assets/illustrations/little_boy.jpeg'),
+                    ),
+                    const SizedBox(height: 32),
+                    Text(
+                      'Welcome,',
+                      style: GoogleFonts.roboto(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w600,
+                        color: const Color(0xff16314d),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Sign in to continue',
+                      style: GoogleFonts.roboto(
+                        fontSize: 20,
+                        color: const Color(0xff16314d),
+                      ),
+                    ),
+                    // Spacer(),
+                    Center(
+                      child: GoogleSignInButton(
+                        onTap: authBloc.signInWithGoogle,
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 32),
-                const LogoAndSyncademic(),
-                const SizedBox(height: 32),
-                SignInButton(
-                  Buttons.google,
-                  onPressed: authBloc.signInWithGoogle,
-                ),
-              ],
+              ),
             )),
           );
 

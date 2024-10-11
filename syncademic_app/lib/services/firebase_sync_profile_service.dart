33b88ae554a62dabfcf2ed6f55a3db2624bbb82b
Samuel Mallet <69467005+SuperMuel@users.noreply.github.com>
2024-10-11 17:55:24 +0200

    Front: request full sync

--- syncademic_app/lib/services/firebase_sync_profile_service.dart
+++ syncademic_app/lib/services/firebase_sync_profile_service.dart
@@ -5,11 +5,15 @@ import 'sync_profile_service.dart';
 
 class FirebaseSyncProfileService implements SyncProfileService {
   @override
-  Future<void> requestSync(String syncProfileId) async {
-    log('Requesting sync for $syncProfileId');
+  Future<void> requestSync(
+    String syncProfileId, {
+    SynchronizationType synchronizationType = SynchronizationType.regular,
+  }) async {
+    log('Requesting $synchronizationType sync for $syncProfileId');
     try {
       await FirebaseFunctions.instance.httpsCallable('request_sync').call({
         'syncProfileId': syncProfileId,
+        'syncType': synchronizationType.name,
       });
     } on FirebaseFunctionsException catch (e) {
       log('Error requesting sync');
--- syncademic_app/lib/services/sync_profile_service.dart
+++ syncademic_app/lib/services/sync_profile_service.dart
@@ -1,12 +1,21 @@
 import 'dart:developer';
 
+enum SynchronizationType {
+  regular,
+  full,
+}
+
 abstract class SyncProfileService {
-  Future<void> requestSync(String syncProfileId);
+  Future<void> requestSync(String syncProfileId,
+      {SynchronizationType synchronizationType = SynchronizationType.regular});
 }
 
 class MockSyncProfileService implements SyncProfileService {
   @override
-  Future<void> requestSync(String syncProfileId) async {
-    log('Requesting sync for $syncProfileId using a mock service');
+  Future<void> requestSync(
+    String syncProfileId, {
+    SynchronizationType synchronizationType = SynchronizationType.regular,
+  }) async {
+    log('Requesting sync for $syncProfileId using a mock service (type: $synchronizationType)');
   }
 }

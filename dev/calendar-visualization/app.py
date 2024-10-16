c43c376eb775dff688a24a468e00e0362c9e5df2
Samuel Mallet <69467005+SuperMuel@users.noreply.github.com>
2024-10-16 15:45:11 +0200

    color visualization

--- dev/calendar-visualization/app.py
+++ dev/calendar-visualization/app.py
@@ -1,3 +1,4 @@
+from functions.shared.google_calendar_colors import GoogleEventColor
 import streamlit as st
 from functions.shared.event import Event
 from functions.synchronizer.ics_parser import IcsParser
@@ -12,15 +13,18 @@ def load_events_from_url(url: str) -> list[Event]:
     return IcsParser().parse(loader.get_ics_string())
 
 
-def events_to_calendar_events(events: list[Event]) -> list[dict]:
-    return [
-        {
-            "title": event.title,
-            "start": event.start.isoformat(),
-            "end": event.end.isoformat(),
-        }
-        for event in events
-    ]
+def event_to_calendar_event(event: Event) -> dict:
+    # https://fullcalendar.io/docs/event-object
+    d = {
+        "title": event.title,
+        "start": event.start.isoformat(),
+        "end": event.end.isoformat(),
+    }
+
+    if event.color:
+        d["backgroundColor"] = event.color.to_color_code()
+
+    return d
 
 
 with st.sidebar:
@@ -32,7 +36,9 @@ with st.sidebar:
             st.success(f"Loaded {len(events)} events from ICS")
 
 if "events" in st.session_state:
-    calendar_events = events_to_calendar_events(st.session_state.events)
+    calendar_events = [
+        event_to_calendar_event(event) for event in st.session_state.events
+    ]
 
     calendar_options = {
         "editable": "false",
